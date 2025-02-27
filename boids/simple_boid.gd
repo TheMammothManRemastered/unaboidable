b9bf08f727ba6d1a0825c7e600f58988da3130a9
Will Owens <wowens4@gmu.edu>
2025-02-27 11:38:34 -0500

    why is it so odd

--- boids/simple_boid.gd
+++ boids/simple_boid.gd
@@ -3,13 +3,13 @@ class_name SimpleBoid
 
 # NOTE: these all need to be defined in any class that extends Boid
 # there's 10000% a better way to do this, fix that lol
-static var max_speed: float = 70.0
-static var separation_radius: float = 40
+static var max_speed: float = 300.0
+static var separation_radius: float = 80
 static var separation_weight: float = 30
-static var alignment_radius: float = 75
-static var alignment_weight: float = 0.2
-static var cohesion_radius: float = 150
-static var cohesion_weight: float = 0.15
+static var alignment_radius: float = 125
+static var alignment_weight: float = 20
+static var cohesion_radius: float = 250
+static var cohesion_weight: float = 25
 static var discriminatory: bool = true
 
 static var class_id = 0
