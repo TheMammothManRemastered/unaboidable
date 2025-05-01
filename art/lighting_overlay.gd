1a80076cbdcb70e2079030759f8a75adf2099dea
Justin1L8 <justin.w.langdon@gmail.com>
2025-05-01 00:37:12 -0400

    Reset game on death (scuffed implementation)

--- art/lighting_overlay.gd
+++ art/lighting_overlay.gd
@@ -5,6 +5,12 @@ extends TextureRect
 func _process(delta: float) -> void:
 	if Engine.get_frames_drawn() % 4 == 0:
 		random_move()
+	
+	if Player.instance.is_dead:
+		create_tween().tween_property(self, "modulate", Color.BLACK, 2.0)
+		if modulate.r < .1:
+			get_tree().reload_current_scene()
+			Engine.time_scale = 1.0
 
 func random_move() -> void:
 	position = Vector2(randf_range(-1, 1), randf_range(-1, 1)) * 20 + base_position
